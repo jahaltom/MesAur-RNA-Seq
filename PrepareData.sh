0bab1bed059b977fa53c3ea90e37d235958edeb7
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-10-13 17:58:25 -0500

    Create PrepareData.sh

---
+++ PrepareData.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --time=3:00:00   
+#SBATCH --nodes=1  
+#SBATCH --ntasks-per-node=16   
+
+
+mkdir Rat_data
+cd Rat_data
+
+
+#cdna.all' - all transcripts of Ensembl genes, excluding ncRNA.
+wget http://ftp.ensembl.org/pub/release-107/fasta/mesocricetus_auratus/cdna/Mesocricetus_auratus.MesAur1.0.cdna.all.fa.gz
+
+#These files hold the transcript sequences corresponding to non-coding RNA genes (ncRNA)
+wget http://ftp.ensembl.org/pub/release-107/fasta/mesocricetus_auratus/ncrna/Mesocricetus_auratus.MesAur1.0.ncrna.fa.gz
+
+
+#Get Mesocricetus auratus Genome
+wget http://ftp.ensembl.org/pub/release-107/fasta/mesocricetus_auratus/dna/Mesocricetus_auratus.MesAur1.0.dna.toplevel.fa.gz
+
+#download covid seq
+wget -q ftp://ftp.ensemblgenomes.org/pub/viruses/fasta/sars_cov_2/cdna/Sars_cov_2.ASM985889v3.cdna.all.fa.gz
+
+
+gunzip *gz
+
+
+
+
+
+
+#create decoy list with genome.
+grep ">" Mesocricetus_auratus.MesAur1.0.dna.toplevel.fa | cut -d " " -f 1 | tr -d ">" > decoys.txt
+
+#combine transcriptomes and decoy fasta files.
+cat Mesocricetus_auratus.MesAur1.0.cdna.all.fa Mesocricetus_auratus.MesAur1.0.ncrna.fa Sars_cov_2.ASM985889v3.cdna.all.fa Mesocricetus_auratus.MesAur1.0.dna.toplevel.fa > MesAur_sars_tx_decoy.fasta
+        
+
+#cleanup
+rm Mesocricetus_auratus.MesAur1.0.cdna.all.fa Mesocricetus_auratus.MesAur1.0.ncrna.fa Sars_cov_2.ASM985889v3.cdna.all.fa Mesocricetus_auratus.MesAur1.0.dna.toplevel.fa
+
+#create salmon index
+time salmon index -t MesAur_sars_tx_decoy.fasta -d decoys.txt -p 16 -i salmon_index
